2acc3479e3e00b9c91d65faa2b32bdf6c999ed89
Ignacio Cruz <igmoweb@gmail.com>
2017-04-27 08:58:41 +0200

    wp_title is not allowed anymore

--- checks/title.php
+++ checks/title.php
@@ -7,42 +7,45 @@
  *
  * See: http://make.wordpress.org/themes/guidelines/guidelines-theme-check/
  */
-class CampusPress_Title_Checks implements CampusPress_themecheck {
-    protected $error = array();
+class Title_Checks implements CampusPress_themecheck {
+	protected $error = array();
 
 	function check( $php_files, $css_files, $other_files ) {
 		$ret = true;
 		$php = implode( ' ', $php_files );
 
-		/**
-		 * Look for <title> and </title> tags.
-		 */
-		campuspress_checkcount();
-		if ( false === strpos( $php, '<title>' ) || false === strpos( $php, '</title>' ) ) {
-			$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check').'</span>: ' . __( 'The theme needs to have <strong>&lt;title&gt;</strong> tags, ideally in the <strong>header.php</strong> file.', 'theme-check' );
+		// Look for add_theme_support( 'title-tag' ) first
+		$titletag = true;
+		if ( ! preg_match( '#add_theme_support\s?\(\s?[\'|"]title-tag#', $php ) ) {
+			$this->error[] = '<span class="tc-lead tc-required">'.__('REQUIRED','theme-check').'</span>: '.__('No reference to <strong>add_theme_support( "title-tag" )</strong> was found in the theme.', 'theme-check' );
+			$titletag = false;
 			$ret = false;
 		}
 
-		/**
-		 * Check whether there is a call to wp_title().
-		 */
+		// Look for <title> and </title> tags.
 		campuspress_checkcount();
-		if ( false === strpos( $php, 'wp_title(' ) ) {
-			$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check').'</span>: ' . __( 'The theme needs to have a call to <strong>wp_title()</strong>, ideally in the <strong>header.php</strong> file.', 'theme-check' );
+		if ( ( 0 <= strpos( $php, '<title>' ) || 0 <= strpos( $php, '</title>' ) ) && !$titletag  ) {
+			$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check').'</span>: ' . __( 'The theme must not used the <strong>&lt;title&gt;</strong> tags.', 'theme-check' );
 			$ret = false;
 		}
 
-		/**
-		 * Check whether the the <title> tag contains something besides a call to wp_title().
-		 */
+		// Check whether there is a call to wp_title()
+		campuspress_checkcount();
+		if ( 0 <= strpos( $php, 'wp_title(' ) && !$titletag ) {
+			$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check').'</span>: ' . __( 'The theme must not call to <strong>wp_title()</strong>.', 'theme-check' );
+			$ret = false;
+		}
+
+		//Check whether the the <title> tag contains something besides a call to wp_title()
 		campuspress_checkcount();
 
 		foreach ( $php_files as $file_path => $file_content ) {
-			/**
-			 * First looks ahead to see of there's <title>...</title>
-			 * Then performs a negative look ahead for <title><?php wp_title(...); ?></title>
-			 */
-			if ( preg_match( '/(?=<title>(.*)<\/title>)(?!<title>\s*<\?php\s*wp_title\([^\)]*\);\s*\?>\s*<\/title>)/s', $file_content ) ) {
+			// Look for anything that looks like <svg>...</svg> and exclude it (inline svg's have titles too)
+			$file_content = preg_replace('/<svg.*>.*<\/svg>/s', '', $file_content);
+
+			// First looks ahead to see of there's <title>...</title>
+			// Then performs a negative look ahead for <title> wp_title(...); </title>
+			if ( preg_match( '/(?=<title>(.*)<\/title>)(?!<title>\s*<\?php\s*wp_title\([^\)]*\);?\s*\?>\s*<\/title>)/s', $file_content ) ) {
 				$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check').'</span>: ' . __( 'The <strong>&lt;title&gt;</strong> tags can only contain a call to <strong>wp_title()</strong>. Use the  <strong>wp_title filter</strong> to modify the output', 'theme-check' );
 				$ret = false;
 			}
@@ -54,4 +57,4 @@ class CampusPress_Title_Checks implements CampusPress_themecheck {
 	function getError() { return $this->error; }
 }
 
-$themechecks[] = new CampusPress_Title_Checks;
+$themechecks[] = new Title_Checks;
